d77d9786b1970f9cc3ab75f4217d642e99ef54a7
erbustos <erbustos@ERBUSTOS-LAP.oradev.oraclecorp.com>
2021-04-21 21:19:45 -0500

    added timestamp support and macid code

--- IrrigationSystem.ino
+++ IrrigationSystem.ino
@@ -2,6 +2,9 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 
+#include <NTPClient.h>
+
+
 #define pin15 15 //moisture sensor
 #define pin2 2  //photoresistor sensor
 #define pin4 4 //Rain Detector
@@ -11,29 +14,48 @@
 const char* ssid     = "IZZI-FAC9";
 const char* password = "704FB841FAC9";
 
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
 //Your Domain name with URL path or IP address with path
 String serverName = "https://project-toolchain-iotpushtodb.mybluemix.net";
 
+// Variables to save date and time
+String formattedDate;
+String dayStamp;
+String timeStamp;
+String macid;
 int moisturevalue, photoresistor_val, rainValue, waterValue, airTemp;
 
 void setup() {
   Serial.begin(115200);
-  connectToWiFi(); //Connect to WiFi
+  connectToWiFi(); //Connect to WiFi 
   //pinMode(RELAY, OUTPUT);  //Relay setup
   //digitalWrite(RELAY, HIGH);
   Serial.println("Configuring sensors setup...");
 
+  timeClient.begin();
+  timeClient.setTimeOffset(-18000);
 }
 
 void loop() {
    delay(10000); // 10 seconds delay
-   
+   while(!timeClient.update()) {
+    timeClient.forceUpdate();
+    }
+
+   // We need to extract date and time
+  formattedDate = timeClient.getFormattedDate();
+
   //Read Sensors
   moisturevalue = analogRead(pin15);
   photoresistor_val= analogRead(pin2);
   rainValue= analogRead(pin4);
   waterValue = analogRead(pin27);
-  
+
+  //Get macid 9C:9C:1F:C7:F7:E4
+  macid= WiFi.macAddress();
   //Post Data to Server
   postToServer();
         
@@ -81,12 +103,14 @@ void postToServer() {
 
       //Set JSON
       StaticJsonDocument<200> jsonDoc;
-      jsonDoc["sensor"] = "gps";
-      jsonDoc["soilHumidity"] = moisturevalue ;
-      jsonDoc["soilHumidity"] = airTemp ;
+      jsonDoc["airHumidity"] = 29.5;
+      jsonDoc["airTemperature"] = 25.8 ;
       jsonDoc["light"] = photoresistor_val ;
+      jsonDoc["soilHumidity"] = moisturevalue ;
       jsonDoc["rainDrops"] = rainValue ;
-
+      jsonDoc["timestamp"] = formattedDate ;
+      jsonDoc["macid"] = macid ;
+      
       String output;
       serializeJson(jsonDoc, output);
       Serial.println(output);
